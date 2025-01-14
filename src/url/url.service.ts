da7532f6335ad8effbc47261daa91f645fdb4009
Sultonbek <kingsulton45@gmail.com>
2025-01-14 14:17:37 +0500

    [BE-2] feat (Создание короткой ссылки и Получение информации о ссылке

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Url } from './url.entity';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class UrlService {
@@ -11,15 +12,32 @@ export class UrlService {
   ) {}
 
   async shorten(originalUrl: string): Promise<Url> {
+    const shortUrl = this.generateShortUrl();
     const newUrl = this.urlRepository.create({
       originalUrl,
-      shortUrl: originalUrl,
+      shortUrl,
     });
 
     return await this.urlRepository.save(newUrl);
   }
 
+  private generateShortUrl(): string {
+    return crypto.randomBytes(3).toString('hex');
+  }
+
   async getInfo(shortUrl: string): Promise<Url | undefined> {
     return await this.urlRepository.findOne({ where: { shortUrl } });
+  }
+
+  async redirectToOriginalUrl(shortUrl: string): Promise<string | undefined> {
+    const url = await this.urlRepository.findOne({ where: { shortUrl } });
+
+    if (url) {
+      url.clickCount += 1;
+      await this.urlRepository.save(url);
+      return url.originalUrl;
+    }
+
+    return undefined;
   }
 }
